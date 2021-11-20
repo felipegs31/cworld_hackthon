80042fdb0d1f87495f4d399d07fa909abc41de6a
Matheus Gaya Scandiffio <matheus@nexmob.com.br>
2021-11-20 18:35:44 -0300

    fix
    fix

--- backend/src/api/campaigns/controller.js
+++ backend/src/api/campaigns/controller.js
@@ -1,6 +1,8 @@
 import { success, notFound } from '../../services/response/'
 import { Campaigns } from '.'
+import { Rewards } from '../rewards'
 import { fetchTwitter, detectSentiment } from './utils'
+import { createReward } from '../rewards/controller'
 
 export const create = ({ bodymen: { body } }, res, next) =>
   Campaigns.create(body)
@@ -14,6 +16,11 @@ export const analyze = ({ bodymen: { body }, params }, res, next) => {
     .then(async (campaign) => {
       const tweets = await fetchTwitter(campaign)
       const tweetsWithSentiment = await detectSentiment(tweets)
+      for (const tweet of tweetsWithSentiment) {
+        if (tweet.sentiment.SentimentScore.Positive >= (campaign.positivity / 100)) {
+          createReward(campaign, tweet)
+        }
+      }
       return tweetsWithSentiment
     })
     .then(success(res))
@@ -37,6 +44,18 @@ export const show = ({ params }, res, next) =>
     .then((campaigns) => campaigns ? campaigns.view() : null)
     .then(success(res))
     .catch(next)
+
+export const showRewards = ({ params, querymen: { query, select, cursor } }, res, next) => {
+  Rewards.count(Object.assign(query, { deleted: false, campaign: params.id }))
+    .then(count => Rewards.find(Object.assign(query, { deleted: false, campaign: params.id }), select, cursor)
+      .then((rewards) => ({
+        count,
+        rows: rewards.map((rewards) => rewards.view())
+      }))
+    )
+    .then(success(res))
+    .catch(next)
+}
 
 export const update = ({ bodymen: { body }, params }, res, next) =>
   Campaigns.findById(params.id)
