d6eb4aad93a81316b2106670426f92e55f465c13
Matheus Gaya Scandiffio <matheus@nexmob.com.br>
2021-11-21 23:51:20 -0300

    final commit
    final commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,6 @@ if (mongo.uri) {
   mongoose.connect(mongo.uri)
 }
 mongoose.Promise = Promise
-
 setImmediate(() => {
   server.listen(port, ip, () => {
     console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env)
